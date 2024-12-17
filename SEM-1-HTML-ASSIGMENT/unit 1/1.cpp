b859a1aa07ef837d21d5ba4113a051bb6e0c3020
dishant729 <dishantnagar05@gmail.com>
2024-12-17 19:28:18 +0530

    Add files via upload

---
+++ SEM-1-HTML-ASSIGMENT/unit 1/1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b;
+  int add,sub,multi,div;
+
+  cout<<"enter a and b";
+  cin>>a>>b;
+
+  int add=a+b;
+  int sub=a-b;
+  int multi=a*b;
+  int div=a/b;
+
+cout<<"addition a and b"<<add<<endl;
+cout<<"subtraction a and b"<<sub<<endl;
+cout<<"multiplication a and b"<<multi<<endl;
+cout<<"division a ad b"<<div<<endl;
+
+return 0;
+
+
+
+
+
+
+}
---
+++ SEM-1-HTML-ASSIGMENT/unit 1/10.js
@@ -0,0 +1,5 @@
+//Write an external JavaScript code to find to create a function to convert Fahrenheit to Celsius
+var f,c=0;
+f = prompt("Enter temprature in farenheit:");
+c=(f-32)*5/9;
+document.write(f+" in celsisus is "+c);
